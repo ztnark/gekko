d604c3538f77686980f334cb9cffacaf054bbd1f
Jeff K <jjkrantz@gmail.com>
2017-12-15 17:30:22 +0000

    update pushbullet

--- plugins/pushbullet.js
+++ plugins/pushbullet.js
@@ -8,24 +8,32 @@ var log = require('../core/log.js');
 var util = require('../core/util.js');
 var config = util.getConfig();
 var pushbulletConfig = config.pushbullet;
+var Manager = require('./trader/portfolioManager');
+var fs = require('fs');
+var inputFile='../profit/eth/target.json';
 
 var Pushbullet = function(done) {
     _.bindAll(this);
 
     this.pusher;
     this.price = 'N/A';
-
+    this.manager = new Manager(_.extend(config.trader, config.watch));
+    this.manager.init(this.setup);
     this.done = done;
-    this.setup();
 };
 
 Pushbullet.prototype.setup = function(done){
-
+    this.manager.setPortfolio(function(){
+	setupPushBullet()
+    });
+    
     var setupPushBullet = function (err, result) {
-        if(pushbulletConfig.sendMessageOnStart){
+	if(pushbulletConfig.sendMessageOnStart){
+            var balance = this.balanceString();
             var title = pushbulletConfig.tag;
             var exchange = config.watch.exchange;
             var currency = config.watch.currency;
+            var target = this.getTarget();
             var asset = config.watch.asset;
             var body = "Gekko has started, Ive started watching "
                 +exchange
@@ -33,14 +41,40 @@ Pushbullet.prototype.setup = function(done){
                 +currency
                 +" "
                 +asset
-                +" I'll let you know when I got some advice";
+                +" target: "
+                +target
+                +" balance: "
+                +balance;
             this.mail(title, body);
         }else{
             log.debug('Skipping Send message on startup')
         }
-    };
-    setupPushBullet.call(this)
+    }.bind(this);
 };
+
+Pushbullet.prototype.balanceString = function(){
+     var string = ""
+     var total = 0
+     var price = this.price
+     _.each(this.manager.portfolio, function(obj) {
+        string +=  " " + obj.name + ": " +  parseFloat(obj.amount).toFixed(2) + " ";
+	if(obj.name === "USD"){
+	    total += parseFloat(obj.amount);
+	}else{
+	    total += parseFloat(obj.amount) * price
+	}
+    });
+    var usd = "$" + total.toFixed(2);
+    var percent = ((total/788-1)*100).toFixed(2);
+    var formattedPercent = percent > 0 ? "+" + percent + "%" : "-" + percent + "%";
+    return string + " (" + usd + " " + formattedPercent + ")";
+}
+
+Pushbullet.prototype.getTarget = function(){
+    var obj = JSON.parse(fs.readFileSync(inputFile, 'utf8'));
+    var target = obj.prediction;
+    return target;
+}
 
 Pushbullet.prototype.processCandle = function(candle, done) {
     this.price = candle.close;
@@ -49,22 +83,33 @@ Pushbullet.prototype.processCandle = function(candle, done) {
 };
 
 Pushbullet.prototype.processAdvice = function(advice) {
-	if (advice.recommendation == "soft" && pushbulletConfig.muteSoft) return;
+	this.manager.setPortfolio(function(){
+	    sendMessage();
+	})
+	var sendMessage = function(){
 
-	var text = [
-        'Gekko is watching ',
-        config.watch.exchange,
-        ' and has detected a new trend, advice is to go ',
-        advice.recommendation,
-        '.\n\nThe current ',
-        config.watch.asset,
-        ' price is ',
-        this.price
-    ].join('');
+  	    if (advice.recommendation == "soft" && pushbulletConfig.muteSoft) return;
+	    var target = this.getTarget();
+	    var balance = this.balanceString();
+	    var text = [
+            'Gekko is watching ',
+            config.watch.exchange,
+            ' and has detected a new trend, advice is to go ',
+            advice.recommendation,
+            '.\n\nThe current ',
+            config.watch.asset,
+            ' price is ',
+            this.price,
+	    ' target is ',
+	    target,
+            ' balance is ',
+	    balance
+            ].join('');
 
-    var subject = pushbulletConfig.tag+' New advice: go ' + advice.recommendation;
+   	    var subject = pushbulletConfig.tag+' New advice: go ' + advice.recommendation;
 
-    this.mail(subject, text);
+    	    this.mail(subject, text);
+	}.bind(this);
 };
 
 Pushbullet.prototype.mail = function(subject, content, done) {
